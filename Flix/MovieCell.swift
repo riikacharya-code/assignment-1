e7813a047930779d2ce4ab1e2f0f0b56ea2e725d
riikacharya-code <racharya@vassar.edu>
2022-02-06 21:21:27 -0500

    First commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Riik Acharya on 2/5/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
